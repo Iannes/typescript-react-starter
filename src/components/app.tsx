af43009bfa61c3a71695a6a024fa23115acb06b0
Iannes <yannispyrou@gmail.com>
2019-08-09 18:22:08 +0100

    Add tslint support

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Store from "../contexts/Store";
-import Layout from "./Layout";
-import "../styles.css";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Store from '../contexts/Store';
+import Layout from './Layout';
+import '../styles.css';
 
 const App = () => {
-  return (
-    <>
-      <CssBaseline />
-      <Layout>
-        <p>Hi</p>
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <CssBaseline />
+            <Layout>
+                <p>Hi</p>
+            </Layout>
+        </>
+    );
 };
 
 const WithStore = () => {
-  return (
-    <Store>
-      <App />
-    </Store>
-  );
+    return (
+        <Store>
+            <App />
+        </Store>
+    );
 };
 
 export default WithStore;
